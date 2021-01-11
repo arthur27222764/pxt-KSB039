8ec049c6b3d9101e84973300ca8f51962df68b30
arthur <arthur@kaise.com.tw>
2021-01-11 08:47:30 +0800

    v0.1.4

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ namespace KSB039 {
     */
     //% blockId=KSB039_Mic
     //% block="Mic"
-    //% weight=98
+    //% weight=100
     export function Mic(): number {
 
         return pins.analogReadPin(AnalogPin.P1);
@@ -23,9 +23,9 @@ namespace KSB039 {
     */
     //% blockId=KSB039_Slider
     //% block="Slider"
-    //% weight=98
+    //% weight=99
     export function Slider(): number {
-        
+
         return pins.analogReadPin(AnalogPin.P2);;
     }
 
@@ -53,19 +53,62 @@ namespace KSB039 {
         return Math.idiv(d, 58);
     }
 
+
     /**
     * P16
     */
-    //% blockId="KSB039_RGB" 
-    //% block="RGB LED "
+    //% blockId="KSB039_RGB1" 
+    //% block="RGB LED1"
     //% weight=96
-    export function RGB_LED(): neopixel.Strip {
+    export function RGB_LED1(): neopixel.Strip {
         if (!neoStrip) {
             neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
         }
 
-        return neoStrip;
+        return neoStrip.range(0, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB2" 
+    //% block="RGB LED2"
+    //% weight=95
+    export function RGB_LED2(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(1, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB3" 
+    //% block="RGB LED3"
+    //% weight=94
+    export function RGB_LED3(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(2, 1);
+    }
+    /**
+    * P16
+    */
+    //% blockId="KSB039_RGB4" 
+    //% block="RGB LED4"
+    //% weight=93
+    export function RGB_LED1(): neopixel.Strip {
+        if (!neoStrip) {
+            neoStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+        }
+
+        return neoStrip.range(3, 1);
     }
 
 
